4590f9ef9de394ae6d7f113b35efa97a4b8d40ae
NguyenHongSon <nguyenson25701@gmail.com>
2022-12-26 23:01:06 +0700

    update#add relationship model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+    
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
 }
